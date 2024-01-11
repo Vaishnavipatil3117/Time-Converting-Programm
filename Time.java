0fe6571210ae096e9e3e91ec4771adffbed43303
Vaishnavipatil3117 <119510001+Vaishnavipatil3117@users.noreply.github.com>
2024-01-11 15:49:56 +0530

    Add files via upload

---
+++ Time.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Time {
+    public static String convertTime(String s) {
+        int hours = Integer.parseInt(s.substring(0, 2));
+        int minutes = Integer.parseInt(s.substring(3, 5));
+        int seconds = Integer.parseInt(s.substring(6, 8));
+        String period = s.substring(9);
+
+        if (period.equals("PM") && hours != 12) {
+            hours += 12;
+        } else if (period.equals("AM") && hours == 12) {
+            hours = 0;
+        }
+
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter time in 12-hour format (hh:mm:ss AM/PM): ");
+        String time = sc.nextLine();
+        String time1 = convertTime(time);
+        System.out.println("The military time is: " + time1);
+    }
+}
